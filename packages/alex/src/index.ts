bff33c80941db7316f16cd6bc2420469ed18b8d1
winjo <winjo.gwj@antfin.com>
2021-01-15 16:28:05 +0800

    fix: build scripts

--- packages/alex/src/index.ts
+++ packages/alex/src/index.ts
@@ -1,1 +1,2 @@
 export * from './api/createApp';
+export * from './api/renderApp';
--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const signale = require('signale');
 const args = require('minimist')(process.argv.slice(2));
+const glob = require('glob');
+const fse = require('fs-extra');
 const { invoke, exec } = require('./utils/utils');
 
 invoke(async () => {
@@ -13,6 +15,17 @@ invoke(async () => {
     const watch = args.w || args.watch ? '--watch' : '';
     await exec(`npx tsc --build packages/tsconfig.build.json ${watch}`);
     signale.success('编译成功');
+    signale.pending('复制非 ts 资源');
+    const cwd = path.join(__dirname, '../packages');
+    const files = glob.sync('*/src/**/*.@(less)', { cwd, nodir: true });
+    console.log(files);
+    for (const file of files) {
+      const from = path.join(cwd, file);
+      const to = path.join(cwd, file.replace(/\/src\//, '/lib/'));
+      await fse.mkdirp(path.dirname(to));
+      await fse.copyFile(from, to);
+    }
+    signale.success('复制成功');
   } catch (err) {
     console.error(err);
     signale.error('编译失败');
