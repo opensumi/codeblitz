8e14730a25b2dae154e118ee4a8ecc474d45fb38
winjo <winjo.gwj@antgroup.com>
2021-05-26 18:03:05 +0800

    fix: 修复 dispose preference reset 问题

--- packages/integrations/src/filesystem/index.tsx
+++ packages/integrations/src/filesystem/index.tsx
@@ -1,59 +1,70 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import { AppRenderer, BrowserFSFileType as FileType, IAppRendererProps } from '@alipay/alex';
 import '@alipay/alex/languages';
-import Button from 'antd/lib/button';
-import 'antd/lib/button/style';
+import Select from 'antd/lib/select';
+import 'antd/lib/select/style';
+
+const dirMap: Record<string, [string, FileType][]> = {
+  '/': [
+    ['lib', FileType.DIRECTORY],
+    ['Readme.md', FileType.FILE],
+    ['LICENSE', FileType.FILE],
+    ['package.json', FileType.FILE],
+  ],
+  '/lib': [
+    ['application.js', FileType.FILE],
+    ['context.js', FileType.FILE],
+    ['request.js', FileType.FILE],
+    ['response.js', FileType.FILE],
+  ],
+};
+
+let zipData: Buffer;
+
+const zipDataPromise = (async () => {
+  const res = await fetch(
+    'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com/green-trail-test/dc85f34d-2467-436b-a0fe-092133ead0d6/demo.zip'
+  );
+  const buf = await res.arrayBuffer();
+  zipData = Buffer.from(new Uint8Array(buf));
+})();
 
 const App = () => {
-  const [zipData, setZipData] = useState<Buffer | null>(null);
   const [fsType, setFsType] = useState<string>('');
 
-  useEffect(() => {
-    (async () => {
-      const res = await fetch(
-        'http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com/green-trail-test/dc85f34d-2467-436b-a0fe-092133ead0d6/demo.zip'
-      );
-      const buf = await res.arrayBuffer();
-      setZipData(Buffer.from(new Uint8Array(buf)));
-    })();
-  }, []);
-
-  const workspace = useMemo<IAppRendererProps['runtimeConfig']['workspace']>(() => {
-    if (fsType === 'ZipFS' && zipData) {
-      return {
-        filesystem: {
-          fs: 'FolderAdapter',
+  const filesystem = useMemo<
+    NonNullable<IAppRendererProps['runtimeConfig']['workspace']>['filesystem'] | undefined
+  >(() => {
+    switch (fsType) {
+      case 'IndexedDB':
+        return {
+          fs: 'IndexedDB',
           options: {
-            folder: '/demo',
-            wrapped: {
-              fs: 'ZipFS',
-              options: {
-                zipData,
-              },
+            storeName: 'my_db',
+          },
+        };
+      case 'InMemory':
+        return {
+          fs: 'InMemory',
+        };
+      case 'FileIndexSystem':
+        return {
+          fs: 'FileIndexSystem',
+          options: {
+            // 初始全量文件索引
+            requestFileIndex() {
+              return Promise.resolve({
+                'main.html': '<div id="root"></div>',
+                'main.css': 'body {}',
+                'main.js': 'console.log("main")',
+                'package.json': '{\n  "name": "Riddle"\n}',
+              });
             },
           },
-        },
-      };
-    }
-    if (fsType === 'DynamicRequest') {
-      const dirMap: Record<string, [string, FileType][]> = {
-        '/': [
-          ['lib', FileType.DIRECTORY],
-          ['Readme.md', FileType.FILE],
-          ['LICENSE', FileType.FILE],
-          ['package.json', FileType.FILE],
-        ],
-        '/lib': [
-          ['application.js', FileType.FILE],
-          ['context.js', FileType.FILE],
-          ['request.js', FileType.FILE],
-          ['response.js', FileType.FILE],
-        ],
-      };
-
-      return {
-        filesystem: {
+        };
+      case 'DynamicRequest':
+        return {
           fs: 'DynamicRequest',
           options: {
             readDirectory(p: string) {
@@ -66,26 +77,74 @@ const App = () => {
               return Buffer.from(await res.arrayBuffer());
             },
           },
-        },
-      };
+        };
+      case 'ZipFS':
+        return {
+          fs: 'ZipFS',
+          options: {
+            zipData,
+          },
+        };
+      case 'FolderAdapter':
+        return {
+          fs: 'FolderAdapter',
+          options: {
+            folder: '/demo',
+            wrapped: {
+              fs: 'ZipFS',
+              options: {
+                zipData,
+              },
+            },
+          },
+        };
+      case 'OverlayFS':
+        return {
+          fs: 'OverlayFS',
+          options: {
+            writable: { fs: 'InMemory' },
+            readable: {
+              fs: 'DynamicRequest',
+              options: {
+                readDirectory(p: string) {
+                  return dirMap[p];
+                },
+                async readFile(p) {
+                  const res = await fetch(
+                    `http://alipay-rmsdeploy-image.cn-hangzhou.alipay.aliyun-inc.com/green-trail-test/a87fb80d-3028-4b19-93a9-2da6f871f369/koa${p}`
+                  );
+                  return Buffer.from(await res.arrayBuffer());
+                },
+              },
+            },
+          },
+        };
     }
-  }, [fsType, zipData]);
+  }, [fsType]);
+
+  const workspace = filesystem ? { filesystem } : undefined;
 
   return (
     <div style={{ height: '100%' }}>
-      <div style={{ height: 32, display: 'flex', alignItems: 'center' }}>
-        <Button size="small" onClick={() => setFsType('ZipFS')}>
-          ZipFS
-        </Button>
-        <Button size="small" style={{ marginLeft: 8 }} onClick={() => setFsType('DynamicRequest')}>
-          DynamicRequest
-        </Button>
+      <div style={{ height: 48, display: 'flex', alignItems: 'center' }}>
+        <Select<string> onChange={(e) => setFsType(e)} style={{ width: 200 }}>
+          <Select.Option value="IndexedDB">IndexedDB</Select.Option>
+          <Select.Option value="InMemory">InMemory</Select.Option>
+          <Select.Option value="FileIndexSystem">FileIndexSystem</Select.Option>
+          <Select.Option value="DynamicRequest">DynamicRequest</Select.Option>
+          <Select.Option value="ZipFS">ZipFS</Select.Option>
+          <Select.Option value="FolderAdapter">FolderAdapter</Select.Option>
+          <Select.Option value="OverlayFS">OverlayFS</Select.Option>
+        </Select>
       </div>
-      <div style={{ height: 'calc(100% - 32px)' }}>
+      <div style={{ height: 'calc(100% - 48px)' }}>
         <AppRenderer
           key={fsType}
           appConfig={{
             workspaceDir: 'filesystem',
+            defaultPreferences: {
+              'general.theme': 'ide-light',
+            },
           }}
           runtimeConfig={{
             biz: 'filesystem',
@@ -97,4 +156,6 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('main'));
+zipDataPromise.then(() => {
+  ReactDOM.render(<App />, document.getElementById('main'));
+});
