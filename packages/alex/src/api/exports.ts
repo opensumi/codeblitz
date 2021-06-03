de5987356c43b644cf400e4ebe978ca244c9d6c8
winjo <winjo.gwj@antgroup.com>
2021-06-03 14:35:33 +0800

    feat: 导出 Uri 和 Emitter

--- packages/alex/src/api/exports.ts
+++ packages/alex/src/api/exports.ts
@@ -1,5 +1,7 @@
 import { REPORT_NAME, BrowserFSFileType, HOME_ROOT, WORKSPACE_ROOT } from '@alipay/alex-core';
 
+export { Uri, Emitter } from '@ali/ide-core-common';
+
 export { getDefaultLayoutConfig } from '../core/layout';
 
 export { REPORT_NAME, BrowserFSFileType, HOME_ROOT, WORKSPACE_ROOT };
