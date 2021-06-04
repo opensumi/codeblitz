e1731cb1bbce9070dd47eb7f3f4f8d0c93f1aa78
winjo <winjo.gwj@antgroup.com>
2021-06-04 14:18:24 +0800

    fix(core): logo 替换为公网地址

--- packages/core/src/common/config.ts
+++ packages/core/src/common/config.ts
@@ -1,6 +1,6 @@
 export const CommonConfig = {
-  logo: 'https://gw-office.alipayobjects.com/bmw-prod/289319bc-d6f4-46e5-b002-20696ea34dd2.svg',
+  logo: 'https://gw.alipayobjects.com/zos/bmw-prod/bd0f02bf-303d-4c1b-ab6f-4fef750aeaac.svg',
   brandName: 'Ant Codespaces',
   productName: 'Ant Codespaces 极速版',
-  icon: 'https://gw-office.alipayobjects.com/bmw-prod/b9229bc1-f4cc-491f-8b0c-26db7c061236.png',
+  icon: 'https://gw.alipayobjects.com/zos/bmw-prod/bd0f02bf-303d-4c1b-ab6f-4fef750aeaac.svg',
 };
