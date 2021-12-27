e165529b6e635b7506e209af1cd74b8221b7af2b
彦熹 <winjo.gwj@antgroup.com>
2021-12-27 16:12:11 +0800

    chore: 修复获取 metadata 接口问题

--- packages/toolkit/webpack/util/scan-extension.js
+++ packages/toolkit/webpack/util/scan-extension.js
@@ -1,34 +1,48 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 const { ExtensionScanner } = require('@ali/ide-kaitian-extension/lib/node/extension.scanner');
 const { getExtension } = require('@alipay/alex-cli/lib/extension/scanner');
 const { Uri } = require('@ali/ide-core-common');
 
-exports.getLocalExtensions = () => {
+/**
+ * @param {string} p
+ */
+const pathExists = (p) =>
+  fs
+    .access(p)
+    .then(() => true)
+    .catch(() => false);
+
+exports.getLocalExtensions = async () => {
   const dirList = [];
   const extensionDir = path.join(__dirname, '../../extensions');
   const fixturesDir = path.join(__dirname, '../../fixtures');
-  if (fs.existsSync(extensionDir)) {
+  if (await pathExists(extensionDir)) {
     dirList.push(extensionDir);
   }
-  if (fs.existsSync(fixturesDir)) {
+  if (await pathExists(fixturesDir)) {
     dirList.push(fixturesDir);
   }
   const extensionScanner = new ExtensionScanner(dirList, 'en-US', [], {});
   return extensionScanner.run();
 };
 
-exports.getLocalExtensionsMetadata = (host, basePath) => {
+exports.getLocalExtensionsMetadata = async (host, basePath) => {
   const dirList = [];
   const extensionDir = path.join(__dirname, '../../extensions');
   const fixturesDir = path.join(__dirname, '../../fixtures');
-  [extensionDir, fixturesDir].forEach((dir) => {
-    fs.readdirSync(dir, { withFileTypes: true }).forEach((dirent) => {
-      if (dirent.isDirectory()) {
-        dirList.push(path.join(dir, dirent.name));
+  await Promise.all(
+    [extensionDir, fixturesDir].map(async (dir) => {
+      if (await pathExists(dir)) {
+        const files = await fs.readdir(dir, { withFileTypes: true });
+        files.forEach((dirent) => {
+          if (dirent.isDirectory()) {
+            dirList.push(path.join(dir, dirent.name));
+          }
+        });
       }
-    });
-  });
+    })
+  );
   const httpUri = Uri.parse(host).with({ path: basePath });
   return Promise.all(dirList.map((localExtPath) => getExtension(localExtPath, 'local', httpUri)));
 };
