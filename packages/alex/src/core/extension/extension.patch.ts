ce5e6fe29fbee36c43d4339da011a06bc363e1e5
xinglong.wangwxl <xinglong.wangwxl@alibaba-inc.com>
2022-06-12 00:20:59 +0800

    fix: 修复加载 webscm 插件时产生多个相同的 reference

--- packages/alex/src/core/extension/extension.patch.ts
+++ packages/alex/src/core/extension/extension.patch.ts
@@ -1,3 +1,4 @@
+import type * as monaco from '@opensumi/monaco-editor-core/esm/vs/editor/editor.api';
 import { MainThreadLanguages } from '@opensumi/ide-extension/lib/browser/vscode/api/main.thread.language';
 import { AbstractExtInstanceManagementService } from '@opensumi/ide-extension/lib/browser/types';
 
@@ -20,6 +21,21 @@ MainThreadLanguages.prototype.createSignatureHelpProvider = function (...args: a
       }
       return v;
     });
+  };
+  return provider;
+};
+
+// TODO: 如果加载 webscm 插件，provideReferences 结果会拿到与当前 file 协议一样的 web_scm 协议的 reference，先过滤掉非 file 协议的文件
+// @ts-ignore
+const _createReferenceProvider = MainThreadLanguages.prototype.createReferenceProvider;
+// @ts-ignore
+MainThreadLanguages.prototype.createReferenceProvider = function (...args: any[]) {
+  const provider = _createReferenceProvider.call(this, ...args);
+  const _provideReferences = provider.provideReferences;
+  provider.provideReferences = function (...args: any[]) {
+    return (_provideReferences.call(this, ...args) as Promise<monaco.languages.Location[]>).then(
+      (references) => references.filter((reference) => reference.uri.scheme === 'file')
+    );
   };
   return provider;
 };
