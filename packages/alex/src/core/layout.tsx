a36b16ff4e77aecde5216d5c4f5b0342289e0520
guqiankun.gqk <guqiankun.gqk@antgroup.com>
2022-07-14 15:18:02 +0800

    chore: 移除默认scm面板

--- packages/alex/src/core/layout.tsx
+++ packages/alex/src/core/layout.tsx
@@ -26,7 +26,7 @@ export const getDefaultLayoutConfig = () => ({
     modules: [''],
   },
   [SlotLocation.left]: {
-    modules: ['@opensumi/ide-explorer', '@opensumi/ide-search', '@opensumi/ide-scm'],
+    modules: ['@opensumi/ide-explorer', '@opensumi/ide-search'],
   },
   [SlotLocation.main]: {
     modules: ['@opensumi/ide-editor'],
