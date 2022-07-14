da5550950a4e0180764e94b3b9171b439877b283
guqiankun.gqk <guqiankun.gqk@antgroup.com>
2022-07-14 15:15:29 +0800

    fix: 全局样式冲突

--- packages/alex/src/api/createEditor.tsx
+++ packages/alex/src/api/createEditor.tsx
@@ -16,7 +16,11 @@ import {
   IReporter,
 } from '@opensumi/ide-core-browser';
 import { BoxPanel, SplitPanel } from '@opensumi/ide-core-browser/lib/components';
-import '@opensumi/ide-core-browser/lib/style/index.less';
+
+import '@opensumi/ide-core-browser/lib/style/entry.less';
+import '@opensumi/ide-core-browser/lib/style/codicons/codicon-animations.css';
+import './normalize.less';
+
 import { IPluginConfig } from '@alipay/alex-plugin';
 import { deletionLogPath } from '@alipay/alex-browserfs/lib/backend/OverlayFS';
 
