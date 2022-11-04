e59558142082eca007030947aaf6e892402a85f7
guqiankun <guqiankun.gqk@antgroup.com>
2022-11-04 12:06:53 +0800

    fix: 修复MonacoContextKeyService绑定错误

--- packages/core/src/client/override/monacoContextKeyService.ts
+++ packages/core/src/client/override/monacoContextKeyService.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@opensumi/di';
 import { IContextKeyService } from '@opensumi/ide-core-browser';
+import { ContextKeyService } from '@opensumi/monaco-editor-core/esm/vs/platform/contextkey/browser/contextKeyService';
 import { MonacoContextKeyService as BaseContextKeyService } from '@opensumi/ide-monaco/lib/browser/monaco.context-key.service';
 // TODO 暂时此处覆盖其 dispose 防止报错
 @Injectable()
 export class MonacoContextKeyService extends BaseContextKeyService implements IContextKeyService {
-  dispose(): void {
-    // 覆盖MonacoContextKeyService
+  public readonly contextKeyService: ContextKeyService;
+
+  constructor() {
+    super();
+    // TODO 不放window
+    // ContextKeyService 全局唯一 组件重置后需重新绑定
+    this.contextKeyService =
+      (window as any)._alex.ContextKeyService || new ContextKeyService(this.configurationService);
+    (window as any)._alex.ContextKeyService = this.contextKeyService;
   }
+
+  dispose(): void {}
 }
