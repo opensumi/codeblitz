f2a8bd8dc1241c76770ed62edce61d6adb54c567
iamkun <kunhello@outlook.com>
2024-05-28 16:17:03 +0800

    fix: export IEditorDocumentModelService to umd bundle

--- packages/core/src/api/exports.ts
+++ packages/core/src/api/exports.ts
@@ -2,6 +2,8 @@ import { REPORT_NAME, BrowserFSFileType, HOME_ROOT, WORKSPACE_ROOT } from '@code
 
 export { Uri, Emitter } from '@opensumi/ide-core-common';
 
+export { IEditorDocumentModelService } from '@codeblitzjs/ide-core/lib/modules/opensumi__ide-editor';
+
 export { getDefaultLayoutConfig } from '../core/layout';
 
 export { REPORT_NAME, BrowserFSFileType, HOME_ROOT, WORKSPACE_ROOT };
