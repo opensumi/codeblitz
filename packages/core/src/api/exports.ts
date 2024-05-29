a3ee5cbdc3ddd74a808388d976c406b7af78f61d
野声 <lijiacheng.ljc@antgroup.com>
2024-05-29 11:33:19 +0800

    fix: remove export

--- packages/core/src/api/exports.ts
+++ packages/core/src/api/exports.ts
@@ -7,8 +7,6 @@ import {
 
 export { Uri, Emitter } from '@opensumi/ide-core-common';
 
-export { IEditorDocumentModelService } from '@opensumi/ide-editor/lib/browser/doc-model/types';
-
 export { getDefaultLayoutConfig } from '../core/layout';
 
 export { REPORT_NAME, BrowserFSFileType, HOME_ROOT, WORKSPACE_ROOT };
