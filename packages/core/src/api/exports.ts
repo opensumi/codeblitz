94d08058150af46c6e1d30d97ddf41ffb36159dc
野声 <lijiacheng.ljc@antgroup.com>
2024-05-29 11:36:16 +0800

    fix: should not export from codeblitz modules (#77)

--- packages/core/src/api/exports.ts
+++ packages/core/src/api/exports.ts
@@ -1,8 +1,11 @@
-import { REPORT_NAME, BrowserFSFileType, HOME_ROOT, WORKSPACE_ROOT } from '@codeblitzjs/ide-sumi-core';
+import {
+  REPORT_NAME,
+  BrowserFSFileType,
+  HOME_ROOT,
+  WORKSPACE_ROOT,
+} from '@codeblitzjs/ide-sumi-core';
 
 export { Uri, Emitter } from '@opensumi/ide-core-common';
-
-export { IEditorDocumentModelService } from '@codeblitzjs/ide-core/lib/modules/opensumi__ide-editor';
 
 export { getDefaultLayoutConfig } from '../core/layout';
 
