bbc2be6b6c51e607a1e406e6a964f01d29517832
野声 <lijiacheng.ljc@antgroup.com>
2024-07-23 20:49:03 +0800

    chore: update export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,6 @@ export * from './api/createApp';
 export * from './api/exports';
 export * from './api/register';
 export * from './api/renderApp';
+export * from './api/renderDiffViewer';
 export * from './api/require';
 export * from './api/types';
