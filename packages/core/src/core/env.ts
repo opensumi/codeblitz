c4f02052f642594164413d92469c52960234b123
野声 <lijiacheng.ljc@antgroup.com>
2024-10-21 11:18:50 +0800

    refactor: build worker host

--- packages/core/src/core/env.ts
+++ packages/core/src/core/env.ts
@@ -1,9 +1,10 @@
-/**
- * worker url
- */
-export const EXT_WORKER_HOST = __WORKER_HOST__;
+import { getResource } from "@codeblitzjs/ide-common";
 
 /**
  * codeblitz version
  */
 export const VERSION = __VERSION__;
+/**
+ * worker url
+ */
+export const EXT_WORKER_HOST = getResource("@codeblitzjs/ide-sumi-core", "resources/worker-host.js", VERSION);
